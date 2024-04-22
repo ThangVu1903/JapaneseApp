8aa361f351e3376a9e184c606f35f6dd95dee16d
ThangVu1903 <vuthang19032002az@gmail.com>
2024-04-22 16:22:53 +0700

    first commit

---
+++ nihongobase/src/main/java/com/japanese/nihongobase/NihongobaseApplication.java
@@ -0,0 +1,13 @@
+package com.japanese.nihongobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihongobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NihongobaseApplication.class, args);
+	}
+
+}
---
+++ nihongobase/src/test/java/com/japanese/nihongobase/NihongobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japanese.nihongobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NihongobaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
